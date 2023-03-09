8f4c76507cab1fd1aaaf9f799436c8067e742f6b
Your Name <you@example.com>
2023-03-09 20:50:10 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Desenvolvendo um site institucional Omma
+
+Site institucional desenvolvido durante o programa XP da gama Academy
+
+## Stacks
+
+- HTML
+- CSS
---
+++ src/scripts/menu.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu')
+const nav2 = document.getElementById('nav2')
+
+menu.addEventListener('click', () =>{
+    nav2.style= 'display: block; position: absolute; background-color: #FFAA00; widht: 100%; height: 40px; top: 10px; left: 20px'
+})
